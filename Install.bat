0b40cbe6a5ae181769760f828148636ad52b6b05
lilHybe <lilhyb3@gmail.com>
2025-01-06 18:34:32 +0100

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -3,9 +3,9 @@ certutil -urlcache -split -f "https://download.microsoft.com/download/1/7/1/1718
 start /wait %USERPROFILE%\Downloads\DirectX.exe
 certutil -urlcache -split -f "https://download.radmin-vpn.com/download/files/Radmin_VPN_1.4.4642.1.exe" %USERPROFILE%\Downloads\RadminVPN.exe
 start /wait %USERPROFILE%\Downloads\RadminVPN.exe
-certutil -urlcache -split -f "https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe"
+certutil -urlcache -split -f "https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe" %USERPROFILE%\Downloads\VCRedist_x64.exe
 start /wait %USERPROFILE%\Downloads\VCRedist_x64.exe
-certutil -urlcache -split -f "https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe"
+certutil -urlcache -split -f "https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe" %USERPROFILE%\Downloads\VCRedist_x86.exe
 start /wait %USERPROFILE%\Downloads\VCRedist_x86.exe
 echo Press any key to leave the script
 pause
